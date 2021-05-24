c2842124694ea1119fd7d600f4069acc42bc101d
Joy Rhee <joyfullyx@gmail.com>
2021-05-24 13:56:14 -0700

    add image path to user and activity models

--- models/User.js
+++ models/User.js
@@ -3,79 +3,86 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 class User extends Model {
-    checkpassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  checkpassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            alowNull: false,
-            validate: {
-                len: [8],
-            },
-        },
-        trip_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "trip",
-                key: "id",
-            },
-        },
-        activities_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "activities",
-                key: "id"
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        hooks: {
-            beforeCreate: async (newUserData) => {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            beforeUpdate: async (updatedUserData) => {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user',
-    }
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      alowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    image_path: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    trip_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "trip",
+        key: "id",
+      },
+    },
+    activities_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "activities",
+        key: "id",
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
 );
 
 module.exports = User;
