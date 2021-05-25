5287b1bd519f62b43602cfc9a59f448ae02aa544
Joy Rhee <joyfullyx@gmail.com>
2021-05-25 14:29:05 -0700

    pull develop

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'; // imports a global reset for css styling
-import Container from '@material-ui/core/Container';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline"; // imports a global reset for css styling
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/box";
+import Button from "@material-ui/core/Button";
+import AddButton from "../components/AddButton";
 
 export default function Dashboard() {
     return (
         <div>
-            <CssBaseline />
-            <Container maxWidth="sm">
-                <h1>Dashboard Page</h1>
-            </Container>
-        </div>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box display="flex">
+          <h1>Trip Page</h1>
+          <Box justifyContent="flex-end">
+          <p>start date</p>
+          <p>end date</p>
+          </Box>
+        </Box>
+        <Button variant="outlined" color="primary">
+          Agenda
+        </Button>
+        <Button variant="outlined" color="primary">
+          Discover
+        </Button>
+        <Button variant="outlined" color="primary">
+          Expenses
+        </Button>
+      </Container>
+      <Container>
+        <AddButton />
+      </Container>
+    </div>
     )
 }
