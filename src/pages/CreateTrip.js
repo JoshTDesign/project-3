4596a28ae883d124400cb4eecb26a8e92a67ca68
Josh Taylor <josh@joshtaylordesign.com>
2021-05-25 07:54:52 -0700

    material ui connected

--- src/pages/CreateTrip.js
+++ src/pages/CreateTrip.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function CreateTrip() {
     return (
         <div>
-            
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <h1>Create Trip Page</h1>
+            </Container>
         </div>
     )
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Dashboard() {
     return (
         <div>
-            
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <h1>Dashboard Page</h1>
+            </Container>
         </div>
     )
 }
