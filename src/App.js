55c3b53e1a03039f3b1a3580947cbac540f9657a
Joy Rhee <joyfullyx@gmail.com>
2021-05-25 14:02:35 -0700

    add button component, start on dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Button from '@material-ui/core/Button'; // imports css framework
 import CssBaseline from '@material-ui/core/CssBaseline'; // imports a global reset for css styling
 import Container from '@material-ui/core/Container';
+import Dashboard from '../src/pages/Dashboard';
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <CssBaseline />
       <Container maxWidth="sm">
         <h1>Home Page</h1>
+        <Dashboard />
       </Container>
     </div>
   );
