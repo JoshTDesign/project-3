3a73954a3c0b0c292a9f90c918aa04057fffc05e
Joy Rhee <joyfullyx@gmail.com>
2021-05-25 23:10:37 -0700

    basic structures for dash components

---
+++ src/components/ProfileTile/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import Box from "@material-ui/core/box";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import { deepPurple } from "@material-ui/core/colors";
+// import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+}));
+
+export default function ProfileTile() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Box p={2} display="inline">
+        <Card>
+          <Avatar
+            alt="Remy Sharp"
+            src="/static/images/avatar/1.jpg"
+            className={classes.large}
+            />
+          <Typography display="inline" variant={"h5"}>Josh Taylor</Typography>
+        </Card>
+      </Box>
+    </div>
+  );
+}
