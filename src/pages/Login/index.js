70c4448849cf89503eb43da9e7196c755094bd72
Josh Taylor <josh@joshtaylordesign.com>
2021-05-27 23:38:38 -0700

    Create User working

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,153 +3,147 @@ import Container from '@material-ui/core/Container';
 import {Button} from '@material-ui/core/';
 import {Grid} from '@material-ui/core/';
 import {TextField} from '@material-ui/core/';
-import {Link} from '@material-ui/core/';
+import { Link, Route } from "react-router-dom";
 import SplashLogo from '../../components/SplashLogo';
 import API from '../../utils/API';
 
 function Login() {
 
+  //States for controlling the form content
+  const [formState,setFormState] = useState({
+    username:"",
+    password:""
+  })
 
-    const [formState,setFormState] = useState({
-      username:"",
-      password:""
-    })
+  const [userState,setUserState] = useState({
+    token:"",
+    user:{
 
-    const [userState,setUserState] = useState({
-      token:"",
-      user:{
-  
-      }
-    })
-  
-    useEffect(()=>{
-      const token = localStorage.getItem("token")
+    }
+  })
+    
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+    if(token){
       API.getProfile(token).then(res=>{
         console.log(res.data);
-      })
-    },[])
-
-
-    const handleInputChange = event => {
-      // Getting the value and name of the input which triggered the change
-      const value = event.target.value;
-      const name = event.target.name;
-  
-      // Updating the input's state
-      setFormState({
-        ...formState,
-        [name]: value
-      });
-    };
-    
-      const handleFormSubmit = event => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        event.preventDefault();
-        API.login(formState).then(res=>{
-          console.log(res.data);
-          localStorage.setItem("token", res.data.token);
-          setUserState({
-            ...userState,
-            token:res.data.token,
-            user:{
-              email:res.data.user.email,
-              username:res.data.user.username,
-            }
-          })
-        }).catch(err=>{
-          console.log("error occured")
-          console.log(err);
+        setUserState({
+          token:token,
+          user:{
+            email:res.data.email,
+            id:res.data.id,
+            username:res.data.username
+          }
         })
-        // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-        setFormState({
-          firstName: "",
-          lastName: ""
-        }, [])
-      }
+    }).catch(err=>{
+      console.log("no logged in user")
+      setUserState({
+        token:"",
+        user:{}
+      })
+    })
+  } else {
+    console.log("no token provided")
+  }
+  },[])
 
-      return (
-        <div>
-            <Container maxWidth="sm">
-                <SplashLogo />
-                <div>
-            <form className="test" onSubmit={handleFormSubmit}>
-                {/* <Grid 
-                container direction="column"
-                justify="center"
-                alignItems="center"> */}
 
-                    <TextField 
-                      className="userName" 
-                      id="outlined-basic" 
-                      label="User Name" 
-                      variant="outlined" 
-                      value={formState.username}
-                      name="username"
-                      onChange={handleInputChange}
-                      placeholder="User Name"
-                      />
+  const handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const value = event.target.value;
+    const name = event.target.name;
 
-                    <TextField 
-                      className="password" 
-                      id="outlined-basic" 
-                      label="Password" 
-                      variant="outlined" 
-                      value={formState.password}
-                      name="password"
-                      onChange={handleInputChange}
-                      placeholder="Password"
-                      />
+    // Updating the input's state
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+      
+  const handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    API.login(formState).then(res=>{
+      console.log(res.data);
+      localStorage.setItem("token", res.data.token);
+      setUserState({
+        ...userState,
+        token:res.data.token,
+        user:{
+          email:res.data.user.email,
+          username:res.data.user.username,
+        }
+      })
+    }).catch(err=>{
+      console.log("error occured")
+      console.log(err);
+      localStorage.removeItem("token");
+    })
+    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+    setFormState({
+      firstName: "",
+      lastName: ""
+    }, [])
+  }
 
-                    <Button 
-                      type="submit"
-                      variant="contained" 
-                      color="primary" 
-                      >
-                        Login
-                    </Button>
+  return (
+    <div>
+        <Container maxWidth="sm">
+            <SplashLogo />
+            <div>
+        <form className="test" onSubmit={handleFormSubmit}>
+            <Grid 
+            container direction="column"
+            justify="center"
+            alignItems="center">
 
-                {/* </Grid> */}
-            </form>
+                <TextField 
+                  className="userName" 
+                  id="outlined-basic" 
+                  label="User Name" 
+                  variant="outlined" 
+                  value={formState.username}
+                  name="username"
+                  onChange={handleInputChange}
+                  placeholder="User Name"
+                  />
 
-                     <p>or</p>
+                <TextField 
+                  className="password" 
+                  id="outlined-basic" 
+                  label="Password" 
+                  variant="outlined" 
+                  value={formState.password}
+                  name="password"
+                  onChange={handleInputChange}
+                  placeholder="Password"
+                  />
 
-                    <Button type="submit" variant="outlined" color="primary">
-                        <Link to="/signup">
-                            Create Account
-                        </Link>
-                    </Button>
-        </div>
-            </Container>
-        </div>
-      )
+                <Button 
+                  type="submit"
+                  variant="contained" 
+                  color="primary" 
+                  >
+                    <Link to="/dashboard">
+                    Login
+                    </Link>
+                </Button>
+
+            </Grid>
+        </form>
+
+                  <p>or</p>
+
+                <Button variant="outlined" color="primary">
+                    <Link to="/signup">
+                        Create Account
+                    </Link>
+                </Button>
+    </div>
+        </Container>
+    </div>
+  )
 }
 
 export default Login;
 
-
-// return (
-//   <div>
-//     <p>
-//       Hello {this.state.firstName} {this.state.lastName}
-//     </p>
-//     <form className="form">
-//       <input
-//         value={this.state.firstName}
-//         name="firstName"
-//         onChange={this.handleInputChange}
-//         type="text"
-//         placeholder="First Name"
-//       />
-//       <input
-//         value={this.state.lastName}
-//         name="lastName"
-//         onChange={this.handleInputChange}
-//         type="text"
-//         placeholder="Last Name"
-//       />
-//       <button onClick={this.handleFormSubmit}>Submit</button>
-//     </form>
-//   </div>
-// );
-// }
-// }
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -3,40 +3,41 @@ import Container from '@material-ui/core/Container';
 import {Button} from '@material-ui/core/';
 import {Grid} from '@material-ui/core/';
 import {TextField} from '@material-ui/core/';
-import {Link} from '@material-ui/core/';
+import { Link, Route } from "react-router-dom";
 import SplashLogo from '../../components/SplashLogo';
+import API from '../../utils/API';
 
-export default function Signup() {
+function Signup() {
 
-const [formState,setFormState] = useState({
+    //States for controlling the form content
+    const [formState,setFormState] = useState({
+    email:"",
     username:"",
     password:""
     })
 
-    const handleCreateAccount = event => {
-        // console.log('create account click');
-    //need code
-    };
-
     const handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const value = event.target.value;
-    const name = event.target.name;
+        // Getting the value and name of the input which triggered the change
+        const value = event.target.value;
+        const name = event.target.name;
 
-    // Updating the input's state
-    setFormState({
-        ... formState,
-        [name]: value
-    });
+        // Updating the input's state
+        setFormState({
+            ... formState,
+            [name]: value
+        });
     };
 
     
     const handleFormSubmit = event => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
         event.preventDefault();
+        API.createUser(formState).then(res=>{
+            console.log(res.data);
+        })
 
         //TODO: should create axios request for user login when form submits
-        console.log("logging in");
+        console.log("creating new user");
     
         // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
         setFormState({
@@ -54,12 +55,23 @@ const [formState,setFormState] = useState({
                 <div>
                 <SplashLogo />
 
-                <form className="test" noValidate autoComplete="off">
+                <form className="test" onSubmit={handleFormSubmit}>
                 <Grid 
                     container direction="column"
                     justify="center"
                     alignItems="center">
 
+                    <TextField 
+                        className="email" 
+                        id="outlined-basic" 
+                        label="Email" 
+                        variant="outlined" 
+                        value={formState.email}
+                        name="email"
+                        onChange={handleInputChange}
+                        placeholder="Email"
+                    />                    
+                    
                     <TextField 
                         className="userName" 
                         id="outlined-basic" 
@@ -82,16 +94,30 @@ const [formState,setFormState] = useState({
                       placeholder="Password"
                       />
                     
-                    <Button variant="contained" color="primary" onClick={handleCreateAccount}>Create Account</Button>
-                    <p>or</p><Button variant="outlined" color="primary">
-                                <Link to="/">
-                                    Login
-                                </Link>
-                            </Button>
+                    <Button 
+                        type="submit"
+                        variant="contained" 
+                        color="primary" 
+                        onClick={console.log('create account')}
+                        >
+                    <Link to="/dashboard">
+                    Create Account
+                    </Link>
+                        </Button>
+
+                    <p>or</p>
+                    
                 </Grid>
                 </form>
+                    <Button variant="outlined" color="primary">
+                        <Link to="/">
+                            Log In
+                        </Link>
+                    </Button>
                 </div>            
         </Container>
         </div>
     )
 }
+
+export default Signup;
