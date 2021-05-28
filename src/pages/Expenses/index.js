48ceb76a30c16e430cf91a34289d3b7a39f5c3f0
Joy Rhee <joyfullyx@gmail.com>
2021-05-28 11:54:14 -0700

    add btn going to appropriate forms

--- src/pages/Expenses/index.js
+++ src/pages/Expenses/index.js
@@ -1,29 +1,51 @@
-import React from 'react'
+import React from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/box";
 import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 import Card from "@material-ui/core/Card";
 import TripHeader from "../../components/TripHeader";
+import { Link } from "react-router-dom";
 
 export default function Expenses() {
-    return (
-        <div>
-            <Container maxWidth="sm">
-                <Paper elevation={3} variant="outlined" style={{ padding: 10 }}>
-                <TripHeader />
-                <Box p={2}>
-                <Card>
-                    <h3>Expenses content goes here</h3>
-                </Card>
-                </Box>
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Paper elevation={3} variant="outlined" style={{ padding: 10 }}>
+          <TripHeader />
+          <Box p={2}>
+            <Card>
+              <h3>Expenses content goes here</h3>
+            </Card>
+          </Box>
 
-                <Box p={2}>
-                <Card>
-                    <h3>More expenses content goes here</h3>
-                </Card>
-                </Box>
-                </Paper>
-            </Container>
-        </div>
-    )
+          <Box p={2}>
+            <Card>
+              <h3>More expenses content goes here</h3>
+            </Card>
+          </Box>
+
+          <div>
+            <Box
+              display="flex"
+              style={{ justifyContent: "flex-end", alignItems: "flex-end" }}
+              m={2}
+            >
+              <Fab
+                color="primary"
+                aria-label="add"
+                // onClick={() => props.addNewTrip("CreateTrip")}
+                component={Link}
+                to="/newexpenseform"
+              >
+                <AddIcon />
+              </Fab>
+            </Box>
+          </div>
+          
+        </Paper>
+      </Container>
+    </div>
+  );
 }
