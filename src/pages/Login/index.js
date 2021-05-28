d3286bd0700d97dda53a6328942daee1ffa6be33
Josh Taylor <josh@joshtaylordesign.com>
2021-05-27 21:17:22 -0700

    user login working

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,6 +5,7 @@ import {Grid} from '@material-ui/core/';
 import {TextField} from '@material-ui/core/';
 import {Link} from '@material-ui/core/';
 import SplashLogo from '../../components/SplashLogo';
+import API from '../../utils/API';
 
 function Login() {
 
@@ -14,10 +15,20 @@ function Login() {
       password:""
     })
 
-    const handleCreateAccount = event => {
-        // console.log('create account click');
-      //need code
-    };
+    const [userState,setUserState] = useState({
+      token:"",
+      user:{
+  
+      }
+    })
+  
+    useEffect(()=>{
+      const token = localStorage.getItem("token")
+      API.getProfile(token).then(res=>{
+        console.log(res.data);
+      })
+    },[])
+
 
     const handleInputChange = event => {
       // Getting the value and name of the input which triggered the change
@@ -26,18 +37,29 @@ function Login() {
   
       // Updating the input's state
       setFormState({
+        ...formState,
         [name]: value
       });
     };
-
     
       const handleFormSubmit = event => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
         event.preventDefault();
-
-        //TODO: should create axios request for user login when form submits
-        console.log("logging in");
-    
+        API.login(formState).then(res=>{
+          console.log(res.data);
+          localStorage.setItem("token", res.data.token);
+          setUserState({
+            ...userState,
+            token:res.data.token,
+            user:{
+              email:res.data.user.email,
+              username:res.data.user.username,
+            }
+          })
+        }).catch(err=>{
+          console.log("error occured")
+          console.log(err);
+        })
         // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
         setFormState({
           firstName: "",
@@ -50,11 +72,11 @@ function Login() {
             <Container maxWidth="sm">
                 <SplashLogo />
                 <div>
-            <form className="test" noValidate autoComplete="off">
-                <Grid 
+            <form className="test" onSubmit={handleFormSubmit}>
+                {/* <Grid 
                 container direction="column"
                 justify="center"
-                alignItems="center">
+                alignItems="center"> */}
 
                     <TextField 
                       className="userName" 
@@ -79,24 +101,23 @@ function Login() {
                       />
 
                     <Button 
+                      type="submit"
                       variant="contained" 
                       color="primary" 
-                      onClick={handleFormSubmit}>
-                        <Link to="/dashboard">
-                            Login
-                        </Link>
+                      >
+                        Login
                     </Button>
+
+                {/* </Grid> */}
+            </form>
 
                      <p>or</p>
 
-                    <Button variant="outlined" color="primary">
+                    <Button type="submit" variant="outlined" color="primary">
                         <Link to="/signup">
                             Create Account
                         </Link>
                     </Button>
-
-                </Grid>
-            </form>
         </div>
             </Container>
         </div>
