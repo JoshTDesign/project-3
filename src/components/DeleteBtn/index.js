8cae922a35f1306a240170a5da73108f90169998
Josh Taylor <josh@joshtaylordesign.com>
2021-06-02 12:43:24 -0700

    agenda page fix

--- src/components/DeleteBtn/index.js
+++ src/components/DeleteBtn/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
+import { Chip } from '@material-ui/core';
+import Clear from '@material-ui/icons/Clear';
+
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
   return (
-    <span className="delete-btn" {...props} role="button" tabIndex="0">
-      ✗
-    </span>
+    <Chip icon={<Clear />} label="Delete Trip" color="primary" {...props} role="button" tabIndex="0">
+    </Chip>
+
   );
 }
 
