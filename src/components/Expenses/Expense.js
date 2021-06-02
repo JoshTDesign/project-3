ee6a9c344f5e72ba1627715411f759bb006602e7
Andrew Yu <igotyuu421@gmail.com>
2021-06-01 23:10:20 -0700

    Expense.js

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -8,46 +8,84 @@ import {
   InputAdornment,
   OutlinedInput,
 } from "@material-ui/core/";
+import { render } from "@testing-library/react";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-      },
-      margin: {
-        margin: theme.spacing(1),
-      },
-      withoutLabel: {
-        marginTop: theme.spacing(3),
-      },
-      textField: {
-        width: '25ch',
-      },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: "25ch",
+  },
 }));
 
 function Expenses() {
-    const classes = useStyles();
-  const [values, setValues] = React.useState({
-    amount: ''
-});
+  const classes = useStyles();
+  const [totalExpenses, setTotalExpenses] = useState();
+  const [inputValue, setInputValue] = useState(null);
+  const [inputActivity, setInputActivity] = useState(null);
+  const [expenses, setExpenses] = useState([]);
 
-return (
-  <TextField
-    label="Size"
-    id="outlined-size-small"
-    defaultValue="Small"
-    variant="outlined"
-    size="small"
-  >
-    <FormControl className={classes.margin} variant="outlined">
-      <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-      <OutlinedInput
-        id="outlined-adornment-amount"
-        value={values.amount}
-        onChange={handleChange("amount")}
-        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-        labelWidth={60}
+  const handleClick = () => {
+    // click should add amount to total amount
+    const newExpense = {
+      value: inputValue,
+      activity: inputActivity,
+    };
+
+    const data = expenses;
+    data.push(newExpense);
+
+    setExpenses([...data]);
+
+    let total = 0;
+    data.forEach((datum) => {
+      total += datum.value;
+    });
+
+    setTotalExpenses(total);
+  };
+  const renderExpensesRow = () => {
+    return expenses.map((expense) => {
+      return (
+        <div>
+          <div>Value: {expense.value}</div>
+          <div>activity: {expense.activity}</div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <>
+      <Box>Total: {totalExpenses}</Box>
+      <TextField
+        label="Name of Activity"
+        id="filled-size-small"
+        value={activity ? activity : ""}
+        onChange={(event) => setInputActivity(event.target.value)}
       />
-    </FormControl>
-  </TextField>
-);
+      <FormControl className={classes.margin} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-amount"
+          onChange={(event) => setInputValue(parseInt(event.target.value))}
+          value={inputValue ? inputValue : 0}
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          labelWidth={60}
+        />
+      </FormControl>
+
+      <Button onClick={handleClick} />
+
+      <div>{renderExpensesRow()}</div>
+    </>
+  );
+}
