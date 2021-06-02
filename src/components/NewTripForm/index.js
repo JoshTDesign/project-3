408b12a4c1bf642e4dfc375e9bd1effa817cdb86
Joy Rhee <joyfullyx@gmail.com>
2021-06-02 11:22:33 -0700

    newtripform form validation

--- src/components/NewTripForm/index.js
+++ src/components/NewTripForm/index.js
@@ -8,6 +8,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import NavBar from "../../components/Navbar";
 import API from "../../utils/API";
+import { ErrorSharp } from "@material-ui/icons";
 
 // TODO: Need to create POST request in form submit handler
 
@@ -21,16 +22,34 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(1),
     width: 200,
   },
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
 }));
+
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
+  return valid;
+};
 
 export default function NewTripForm(props) {
   const [formState, setFormState] = useState({
     tripName: "",
     city: "",
     state: "",
+    country: "",
     start_date: "",
     end_date: "",
+    lodgingUrl: "",
     token: "",
+    errors: {
+      tripName: "",
+      city: "",
+    },
   });
 
   const [userState, setUserState] = useState({
@@ -69,27 +88,90 @@ export default function NewTripForm(props) {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    let tripName = formState.tripName;
+    let city = formState.city;
+    let errors = formState.errors;
 
-    console.log("NewTripForm / formState: ", formState);
-
-    API.createTrip(formState, formState.token).then((res) => {
-      setTripState({
-        ...tripState,
-        userTrips: res.data,
+    if (tripName.length < 1 || city.length < 1) {
+        alert('Please complete required fields')
+      setFormState({
+        errors,
+        tripName: "",
+        city: "",
       });
-      history.push(`/home`);
-    });
-    console.log("NewTripForm / creating new trip!");
+    } else {
+      API.createTrip(formState, formState.token).then((res) => {
+        setTripState({
+          ...tripState,
+          userTrips: res.data,
+        });
+        history.push(`/home`);
+      });
+      console.log("NewTripForm / creating new trip!");
+    }
   };
+
+  //   const handleFormSubmit = (event) => {
+  //     event.preventDefault();
+  //     const { name, value } = event.target;
+  //     let errors = formState.errors;
+
+  //     if (validateForm(formState.errors)) {
+  //       switch (name) {
+  //         case "tripName":
+  //           errors.tripName = value.length < 2 ? "Please enter a trip name" : "";
+  //           break;
+  //         case "city":
+  //           errors.city = value.length < 2 ? "Please enter a valid city" : "";
+  //           break;
+  //         default:
+  //           break;
+  //       }
+  //       setFormState({
+  //         errors,
+  //         tripName: errors.tripName,
+  //         city: errors.city
+  //       });
+  //       console.log("NTF / errors: ", errors);
+  //     } else {
+  //       API.createTrip(formState, formState.token).then((res) => {
+  //         setTripState({
+  //           ...tripState,
+  //           userTrips: res.data,
+  //         });
+  //         history.push(`/home`);
+  //       });
+  //       console.log("NewTripForm / creating new trip!");
+  //     }
+  //   };
+
+  //   const handleFormSubmit = (event) => {
+  //     event.preventDefault();
+
+  //     console.log("NewTripForm / formState: ", formState);
+  //     // if(validateForm(formState.errors)) {
+
+  //     API.createTrip(formState, formState.token).then((res) => {
+  //       setTripState({
+  //         ...tripState,
+  //         userTrips: res.data,
+  //       });
+  //       history.push(`/home`);
+  //     });
+  //     console.log("NewTripForm / creating new trip!");
+  //     // } else {
+  //     // console.error('Invalid form')
+  //   };
 
   const classes = useStyles();
 
   return (
     <div>
       <NavBar />
-      <form className="test" noValidate autoComplete="off">
+      <form className={classes.root} noValidate autoComplete="off">
         <Grid container direction="column" justify="center" alignItems="center">
           <TextField
+            error={validateForm(formState.tripName)}
             className="location"
             id="outlined-basic"
             label="Trip Name"
@@ -98,6 +180,7 @@ export default function NewTripForm(props) {
             name="tripName"
             value={formState.tripName}
             required
+            helperText={!validateForm}
             onChange={handleInputChange}
           />
           <Typography>Start Date</Typography>
@@ -129,6 +212,7 @@ export default function NewTripForm(props) {
             onChange={handleInputChange}
           />
           <TextField
+            error={validateForm(formState.city)}
             className="location"
             id="outlined-basic"
             label="City"
@@ -136,6 +220,7 @@ export default function NewTripForm(props) {
             name="city"
             value={formState.city}
             required
+            // helperText={formState.errors.city}
             onChange={handleInputChange}
           />
           <TextField
@@ -150,7 +235,7 @@ export default function NewTripForm(props) {
           <TextField
             className="location"
             id="outlined-basic"
-            label="Country (optional)"
+            label="Country"
             variant="outlined"
             name="country"
             value={formState.country}
@@ -161,8 +246,8 @@ export default function NewTripForm(props) {
             id="outlined-basic"
             label="Lodging (url)"
             variant="outlined"
-            name="country"
-            value={formState.country}
+            name="lodging"
+            value={formState.url}
             onChange={handleInputChange}
           />
 
